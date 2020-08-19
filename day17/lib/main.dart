d51cb6a8d108a3e3f4d6a1d74c50d7ebaaed225d
Gaurav Goyal <cptn3m0grv@gmail.com>
2020-08-19 13:53:43 +0530

    code update

--- day17/lib/main.dart
+++ day17/lib/main.dart
@@ -1,3 +1,8 @@
+// In this application I have not implemented google maps because I do not have any google API as of now
+// but i will be update this code accordingly as soon as I get it
+// This code will get the current location --> latitude, longitude, altitude and accuracy on a press of a button.
+// *** https://github.com/cptn30grv  ******
+
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
 
@@ -18,8 +23,12 @@ class MyApp extends StatefulWidget{
 
 class _MyApp extends State<MyApp>{
 
-  var lat;
-  var long;
+  var lat = double.infinity;
+  var long = double.infinity;
+  var alt = double.infinity;
+  var acc  = double.infinity;
+  var wid = 300.0;
+  var hei = 230.0;
 
   getLoc() async{
     var position = await Geolocator().getCurrentPosition(
@@ -27,10 +36,12 @@ class _MyApp extends State<MyApp>{
 		);
 
     setState(() {
-      if (lat == null && long == null){
         lat = position.latitude;
         long = position.longitude;
-      } 
+        alt = position.altitude;
+        acc = position.accuracy;
+        wid = 300.0;
+        hei = 350.0;
     });
   }
 
@@ -43,29 +54,41 @@ class _MyApp extends State<MyApp>{
       body: Center(
         child: Container(
           padding: EdgeInsets.all(30),
-          width: 300,
-          height: 300,
+          width: wid,
+          height: hei,
           color: Colors.grey,
           child: Column(
             children: <Widget>[
-                  Text(
-                    "Latitude: $lat",
-                    style: TextStyle(
-                      fontSize: 25,
-                    ),
-                  ),
-                  Text(
-                    "Longitude: $long",
-                    style: TextStyle(
-                      fontSize: 25,
-                    ),
-                  ),
+              Text(
+                "Latitude: $lat deg",
+                style: TextStyle(
+                  fontSize: 25,
+                ),
+              ),
+              Text(
+                "Longitude: $long deg",
+                style: TextStyle(
+                  fontSize: 25,
+                ),
+              ),
+              Text(
+                "Altitude: $alt m",
+                style: TextStyle(
+                  fontSize: 25,
+                ),
+              ),
+              Text(
+                "Accuracy: $acc m",
+                style: TextStyle(
+                  fontSize: 25,
+                ),
+              ),
             ],
           ),
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: getLoc(),
+        onPressed: getLoc,
         child: Icon(Icons.gps_fixed),
       ),
     );
